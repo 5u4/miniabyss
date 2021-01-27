7a68205b8ff041eb3432224a04a85e614a0060c0
Senhung Wong <alexwongsenhung@gmail.com>
2021-01-27 09:57:41 +0800

    Add battle camera

---
+++ Games/BattleCamera.cs
@@ -0,0 +1,28 @@
+using Godot;
+
+namespace MiniAbyss.Games
+{
+    public class BattleCamera : Camera2D
+    {
+        [Export] public NodePath BattleGridPath;
+        public BattleGrid BattleGrid;
+
+        public override void _Ready()
+        {
+            BattleGrid = GetNode<BattleGrid>(BattleGridPath);
+
+            SetLimitsBasedOnBattleGrid();
+        }
+
+        private void SetLimitsBasedOnBattleGrid()
+        {
+            var gRect = BattleGrid.GetUsedRect();
+            var vRect = GetViewportRect();
+            var csize = BattleGrid.CellSize;
+            LimitLeft = Mathf.FloorToInt(Mathf.Min(vRect.Position.x, gRect.Position.x * csize.x));
+            LimitRight = Mathf.FloorToInt(Mathf.Max(vRect.End.x, gRect.End.x * csize.x));
+            LimitTop = Mathf.FloorToInt(Mathf.Min(vRect.Position.y, gRect.Position.y * csize.y));
+            LimitBottom = Mathf.FloorToInt(Mathf.Min(vRect.End.y, gRect.End.y * csize.y));
+        }
+    }
+}
