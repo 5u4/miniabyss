285040d5a843e4d69283679347d8f310122f4bc5
Senhung Wong <alexwongsenhung@gmail.com>
2021-01-29 02:09:05 +0800

    Improve camera following

--- Games/BattleCamera.cs
+++ Games/BattleCamera.cs
@@ -4,26 +4,18 @@ namespace MiniAbyss.Games
 {
     public class BattleCamera : Camera2D
     {
-        [Export] public NodePath BattleGridPath;
-        public BattleGrid BattleGrid;
+        [Export] public NodePath FollowingPath;
+
+        public Node2D Following;
 
         public override void _Ready()
         {
-            BattleGrid = GetNode<BattleGrid>(BattleGridPath);
-
-            // BattleGrid.Connect(nameof(BattleGrid.GenerateMapSignal), this, nameof(SetLimitsBasedOnBattleGrid));
-            // SetLimitsBasedOnBattleGrid();
+            Following = GetNode<Node2D>(FollowingPath);
         }
 
-        private void SetLimitsBasedOnBattleGrid()
+        public override void _PhysicsProcess(float delta)
         {
-            var gRect = BattleGrid.GetUsedRect();
-            var vRect = GetViewportRect();
-            var csize = BattleGrid.CellSize;
-            LimitLeft = Mathf.FloorToInt(Mathf.Min(vRect.Position.x, gRect.Position.x * csize.x));
-            LimitRight = Mathf.FloorToInt(Mathf.Max(vRect.End.x, gRect.End.x * csize.x));
-            LimitTop = Mathf.FloorToInt(Mathf.Min(vRect.Position.y, gRect.Position.y * csize.y));
-            LimitBottom = Mathf.FloorToInt(Mathf.Max(vRect.End.y, gRect.End.y * csize.y));
+            if (Following != null) GlobalPosition = Following.GlobalPosition;
         }
     }
 }
